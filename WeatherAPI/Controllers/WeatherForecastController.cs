3f95189544d4e3ed086933268204a0f99bbbe234
Mike Be <autofakt5@gmail.com>
2021-10-25 12:50:23 -0700

    initial 2

--- WeatherAPI/Controllers/WeatherForecastController.cs
+++ WeatherAPI/Controllers/WeatherForecastController.cs
@@ -28,7 +28,7 @@ namespace WeatherAPI.Controllers
         [Authorize]
         public IEnumerable<WeatherForecast> Get()
         {
-            var rng = new Random();
+            Random rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
